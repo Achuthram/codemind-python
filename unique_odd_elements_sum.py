c228c95cb354585390828b8d1d2351e98a9b5ff2
Achuthram <111500607+Achuthram@users.noreply.github.com>
2023-01-23 17:13:57 +0530

    n=int(input()) l=list(map(int,input().split())) s=0 arr=[] for i in l:     if i not in arr:         arr.append(i) for j in arr:     if j%2!=0:         s=s+j print(s)

---
+++ unique_odd_elements_sum.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=0
+arr=[]
+for i in l:
+    if i not in arr:
+        arr.append(i)
+for j in arr:
+    if j%2!=0:
+        s=s+j
+print(s)
+        
