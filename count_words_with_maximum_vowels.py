15df72e3df27caaac389f9888ed324c637f7dc48
Achuthram <111500607+Achuthram@users.noreply.github.com>
2023-02-04 17:25:41 +0530

    l=list(map(str,input().split())) k=['a','e','i','o','u'] m=[] t=0 for i in l:     s=0     for j in i:         if j in k:             s+=1     m.append(s) m1=max(m) for e in l:     q=0     for f in e:         if f in k:             q+=1     if q==m1:         t+=1 print(t)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,19 @@
+l=list(map(str,input().split()))
+k=['a','e','i','o','u']
+m=[]
+t=0
+for i in l:
+    s=0
+    for j in i:
+        if j in k:
+            s+=1
+    m.append(s)
+m1=max(m)
+for e in l:
+    q=0
+    for f in e:
+        if f in k:
+            q+=1
+    if q==m1:
+        t+=1
+print(t)
